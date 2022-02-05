44ac47aac488a83d38063b700f7bae571865d067
LaDell Erby <ladellerby@gmail.com>
2022-02-05 01:45:32 -0800

    removing boiler plate

--- models/shop-page.ts
+++ models/shop-page.ts
@@ -14,20 +14,10 @@ export class ShopPage {
   }
 
   async addProductsToCart(itemCount: number) {
-    // Click [aria-label="Add\ “Old\ Family\ Purple”\ to\ your\ cart"]
-    // const buttons = await this.page.locator('[aria-label*="to\\ your\\ cart"]');
-
-    // for (let i = 0; i < itemCount - 1; i++) {
-    //   await buttons[i].click();
-    // }
-
-    // Click [aria-label="Add\ “Grape\ Pie”\ to\ your\ cart"]
     await this.page.click(
       '[aria-label="Add\\ “Grape\\ Pie”\\ to\\ your\\ cart"]'
     );
-
-    // Click text=my bag (2)
     await this.page.click(`text=my bag (${itemCount})`);
-    await expect(this.page).toHaveURL('https://staging.710labs.com/cart/');
+    await expect(this.page).toHaveURL('/cart/');
   }
 }
