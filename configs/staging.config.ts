3ba240da81cf61ddce7fee7544d1013c91204e2f
ladellerby <ladellerby@gmail.com>
2022-02-14 01:06:35 -0800

    - maxing out workers for CI run.

--- configs/staging.config.ts
+++ configs/staging.config.ts
@@ -24,7 +24,7 @@ const config: PlaywrightTestConfig = {
   retries: process.env.CI ? 2 : 0,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  //workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
