a7305d66e44d6a1b525fe384821f75589b966c94
ladellerby <ladellerby@gmail.com>
2022-02-14 08:33:17 -0800

    - updating parallel settigns for ci run

--- configs/dev.config.ts
+++ configs/dev.config.ts
@@ -7,7 +7,7 @@ const config: PlaywrightTestConfig = {
   testDir: './../tests',
 
   /* Maximum time one test can run for. */
-  timeout: 120 * 1000,
+  timeout: 60 * 1000,
 
   expect: {
     /**
@@ -24,7 +24,7 @@ const config: PlaywrightTestConfig = {
   retries: process.env.CI ? 3 : 0,
 
   /* Opt out of parallel tests on CI. */
-  //workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 6 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
