03d611e73d7e02e4f7bac35cffe951d2b92999ef
ladellerby <ladellerby@gmail.com>
2022-05-19 10:01:05 -0700

    - updating gh actions - adding basic acceptance test - adding tagging to tests

---
+++ tests/basic-acceptance-test-ca.spec.ts
@@ -0,0 +1,67 @@
+import { test, expect } from '@playwright/test'
+import { ListPasswordPage } from '../models/list-password-protect-page'
+import { AgeGatePage } from '../models/age-gate-page'
+import { ShopPage } from '../models/shop-page'
+import { CreateAccountPage } from '../models/create-account-page'
+import { CheckoutPage } from '../models/checkout-page'
+import { CartPage } from '../models/cart-page'
+import { MyAccountPage } from '../models/my-account-page'
+import { AdminLogin } from '../models/admin-login-page'
+import { OrderReceivedPage } from '../models/order-recieved-page'
+import { EditOrderPage } from '../models/edit-order-page'
+import { SchedulingPage } from '../models/scheduling-page'
+import { v4 as uuidv4 } from 'uuid'
+
+test.describe('Admin Split Order', () => {
+	const zipCode = '90210'
+	const orderQuanity = 6
+	var orderTotals
+	var orderNumber
+
+	test.beforeEach(async ({ page, browserName }, workerInfo) => {
+		test.skip(workerInfo.project.name === 'mobile-chrome')
+		const ageGatePage = new AgeGatePage(page)
+		const listPassword = new ListPasswordPage(page)
+		const createAccountPage = new CreateAccountPage(page)
+		const shopPage = new ShopPage(page, browserName, workerInfo)
+		const cartPage = new CartPage(page, browserName, workerInfo, 1)
+		const schedulingPage = new SchedulingPage(page)
+		const orderReceived = new OrderReceivedPage(page)
+		const checkOutPage = new CheckoutPage(page)
+		const myAccountPage = new MyAccountPage(page)
+
+		await ageGatePage.passAgeGate()
+		await listPassword.submitPassword('qatester')
+		await createAccountPage.create(`test+${uuidv4()}@710labs.com`, 'test1234!', zipCode, 1)
+		if (process.env.ADD_ADDRESS_BEFORE_CHECKOUT === 'true') {
+			await myAccountPage.addAddress()
+		}
+		await shopPage.addProductsToCart(6)
+		var cartTotals = await cartPage.verifyCart(zipCode)
+		await test.step('Verify Checkout Page Totals + Taxes', async () => {
+			orderTotals = await checkOutPage.confirmCheckout(zipCode, cartTotals, 1, true)
+		})
+		await test.step('Schedule Delivery', async () => {
+			await schedulingPage.scheduleDelivery()
+		})
+
+		await test.step('Comfirm Order Details on /order-received', async () => {
+			orderNumber = await orderReceived.confirmOrderDetail(orderTotals)
+			await expect(orderNumber, 'Failed to create order').not.toBeNull()
+		})
+
+		await test.step('Logout Consumer', async () => {
+			await myAccountPage.logout()
+		})
+	})
+	test(`Basic Acceptance Test @CA @BAT`, async ({ page, browserName }, workerInfo) => {
+		const adminLoginPage = new AdminLogin(page)
+		const editOrderPage = new EditOrderPage(page)
+		await test.step('Login Admin', async () => {
+			await adminLoginPage.login()
+		})
+		await test.step('Admin Split Order', async () => {
+			await editOrderPage.splitOrder(orderNumber, orderQuanity)
+		})
+	})
+})
--- tests/list-password-tests.spec.ts
+++ tests/list-password-tests.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test'
 import { AgeGatePage } from '../models/age-gate-page'
 import { ListPasswordPage } from '../models/list-password-protect-page'
 
-test('Enter List Password - Valid', async ({ page }) => {
+test('Enter List Password - Valid @FullRun', async ({ page }) => {
 	const ageGatePage = new AgeGatePage(page)
 	const listPasswordPage = new ListPasswordPage(page)
 
@@ -12,7 +12,7 @@ test('Enter List Password - Valid', async ({ page }) => {
 	await expect(page).toHaveURL('/my-account/')
 })
 
-test('Enter List Password - Invalid', async ({ page }) => {
+test('Enter List Password - Invalid @FullRun', async ({ page }) => {
 	const ageGatePage = new AgeGatePage(page)
 	const listPasswordPage = new ListPasswordPage(page)
 
