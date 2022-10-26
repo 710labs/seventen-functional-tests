0d76a00ab7f5fe04dc38e744f478026223781501
ladellerby <ladellerby@gmail.com>
2022-10-25 23:36:11 -0700

    -adding tesults

--- configs/local.config.ts
+++ configs/local.config.ts
@@ -1,46 +1,50 @@
-import { PlaywrightTestConfig, devices } from '@playwright/test';
+import { PlaywrightTestConfig, devices } from '@playwright/test'
 require('dotenv').config({ path: require('find-config')('.env') })
 
 /* https://playwright.dev/docs/test-configuration */
 const config: PlaywrightTestConfig = {
-  testDir: './../tests',
-  timeout: 60 * 1000,
-  expect: {
-    timeout: 5000,
-  },
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 3 : 0,
-  workers: process.env.CI ? 6 : undefined,
-  reporter: 'html',
-  use: {
-    acceptDownloads: true,
-    actionTimeout: 0,
-    baseURL: process.env.BASE_URL,
-    launchOptions: {
-      slowMo: 200,
-    },
-    trace: 'on',
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
-    {
-      name: 'mobile-chrome',
-      use: {
-        ...devices['Pixel 5'],
-      },
-    },
-    {
-      name: 'mobile-safari',
-      use: {
-        ...devices['iPhone 12'],
-      },
-    },
-  ],
-  outputDir: 'test-results/',
-};
-export default config;
+	testDir: './../tests',
+	timeout: 300 * 1000,
+	expect: {
+		timeout: 10 * 1000,
+	},
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 1 : 0,
+	workers: process.env.CI ? 12 : undefined,
+	reporter: [
+		['list'],
+		['html'],
+		['playwright-tesults-reporter', { 'tesults-target': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjM0ZTE3ODMzLWExOTItNDQxNS1hNjI0LTM1ZThjMGNmZmE2ZC0xNjY2NzYzMjEzNjE2IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiNjY2ZjFmZGYtODY5Yi00MWRlLTk2ZGItNjBlNjI5OTdhZmQ0IiwidHlwZSI6InQifQ.4BjX_l1WjT0zaUNtXlD-upc2i7KbchKnngCXoA0EXUo' }],
+	],
+	use: {
+		acceptDownloads: true,
+		actionTimeout: 300 * 1000,
+		baseURL: 'https://thelist-dev.710labs.com',
+		launchOptions: {
+			slowMo: 200,
+		},
+		trace: 'retain-on-failure',
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+			},
+		},
+		{
+			name: 'mobile-chrome',
+			use: {
+				...devices['Pixel 5'],
+			},
+		},
+		// {
+		//   name: 'mobile-safari',
+		//   use: {
+		//     ...devices['iPhone 12'],
+		//   },
+		// },
+	],
+	outputDir: '../test-results/',
+}
+export default config
