10619b84baa99044e6b5c9cf347695bfb69761ad
ladellerby <ladellerby@gmail.com>
2022-12-01 02:13:44 -0800

    - added playwright congi at root level for vs code playwright extension. - Add error logging when acuity slot selection fails - upgraded playuwright to 1.28.1

---
+++ playwright.config.ts
@@ -0,0 +1,56 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test'
+require('dotenv').config({ path: require('find-config')('.env') })
+
+/* https://playwright.dev/docs/test-configuration */
+const config: PlaywrightTestConfig = {
+	testDir: 'tests',
+	timeout: 300 * 1000,
+	expect: {
+		timeout: 10 * 1000,
+	},
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 1 : 0,
+	workers: process.env.CI ? 12 : undefined,
+	reporter: [
+		['list'],
+		['html'],
+		[
+			'playwright-tesults-reporter',
+			{
+				'tesults-target':
+					'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjM0ZTE3ODMzLWExOTItNDQxNS1hNjI0LTM1ZThjMGNmZmE2ZC0xNjY2NzYzMjEzNjE2IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiNjY2ZjFmZGYtODY5Yi00MWRlLTk2ZGItNjBlNjI5OTdhZmQ0IiwidHlwZSI6InQifQ.4BjX_l1WjT0zaUNtXlD-upc2i7KbchKnngCXoA0EXUo',
+			},
+		],
+	],
+	use: {
+		acceptDownloads: true,
+		actionTimeout: 300 * 1000,
+		baseURL: 'https://thelist-dev.710labs.com',
+		launchOptions: {
+			slowMo: 200,
+		},
+		trace: 'retain-on-failure',
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+			},
+		},
+		{
+			name: 'mobile-chrome',
+			use: {
+				...devices['Pixel 5'],
+			},
+		},
+		// {
+		//   name: 'mobile-safari',
+		//   use: {
+		//     ...devices['iPhone 12'],
+		//   },
+		// },
+	],
+	outputDir: '../test-results/',
+}
+export default config
