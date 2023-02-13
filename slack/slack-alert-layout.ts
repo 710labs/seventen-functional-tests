8266fa061340786e3787d35cadf86a3beb80ee82
ladellerby <ladellerby@gmail.com>
2023-02-12 22:48:29 -0800

    - update slack reporter. switch to slack persistence

--- slack/slack-alert-layout.ts
+++ slack/slack-alert-layout.ts
@@ -2,23 +2,14 @@ import { Block, KnownBlock } from '@slack/types';
 import { SummaryResults } from "playwright-slack-report/dist/src";
 import fs from "fs";
 import path from "path";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 const web_api_1 = require('@slack/web-api');
 const slackClient = new web_api_1.WebClient(process.env.SLACK_BOT_USER_OAUTH_TOKEN);
 
 
-const s3Client = new S3Client({
-    credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY || "",
-        secretAccessKey: process.env.S3_SECRET || "",
-    },
-    region: process.env.S3_REGION,
-});
-
 async function uploadFile(filePath) {
     try {
         const result = await slackClient.files.uploadV2({
-            channels: 'you_cannel_name',
+            channels: 'tech-savagery-tests',
             file: fs.createReadStream(filePath),
             filename: filePath.split('/').at(-1),
         });
@@ -65,7 +56,7 @@ export async function generateCustomLayoutAsync(summaryResults: SummaryResults):
                     if (file) {
                         if (a.name === 'screenshot' && file.permalink) {
                             assets.push({
-                                alt_text: reason,
+                                alt_text: '',
                                 image_url: file.permalink,
                                 title: { type: 'plain_text', text: file.name || '' },
                                 type: 'image',
@@ -74,73 +65,78 @@ export async function generateCustomLayoutAsync(summaryResults: SummaryResults):
 
                         if (a.name === 'video' && file.permalink) {
                             assets.push({
-                                alt_text: reason,
+                                alt_text: '',                               
                                 thumbnail_url: process.env.DEFAULT_VIDEO_PREVIEW_URL,
                                 title: { type: 'plain_text', text: file.name || '' },
                                 type: 'video',
                                 video_url: file.permalink,
                             });
                         }
+                        if (a.name === 'trace' && file.permalink) {
+                            assets.push({
+                                type: "context",
+                                elements: [{ type: "mrkdwn", text: `$<${file.permalink}|${a.name}>` }],
+                            });
+                        }
                     }
                 }
             }
             if (assets.length > 0) {
-                fails.concat(assets)
-            };
+                fails.concat(assets);
+            }
+            if (i > maxNumberOfFailures) {
+                fails.push({
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '*There are too many failures to display, view the full results in BuildKite*',
+                    },
+                });
+                break;
+            }
         }
-        if (i > maxNumberOfFailures) {
-            fails.push({
+
+    }
+
+    if (summaryResults.meta) {
+        for (let i = 0; i < summaryResults.meta.length; i += 1) {
+            const { key, value } = summaryResults.meta[i];
+            meta.push({
                 type: 'section',
                 text: {
                     type: 'mrkdwn',
-                    text: '*There are too many failures to display, view the full results in BuildKite*',
+                    text: `\n*${key}* :\t${value}`,
                 },
             });
-            break;
         }
     }
-
-}
-
-if (summaryResults.meta) {
-    for (let i = 0; i < summaryResults.meta.length; i += 1) {
-        const { key, value } = summaryResults.meta[i];
-        meta.push({
+    return [
+        {
+            type: "header",
+            text: {
+                type: "plain_text",
+                text: "🎭 *710 Labs Test Results*",
+                emoji: true,
+            },
+        },
+        ...meta,
+        {
             type: 'section',
             text: {
                 type: 'mrkdwn',
-                text: `\n*${key}* :\t${value}`,
+                text: `:white_check_mark: *${summaryResults.passed
+                    }* Tests ran successfully \n\n :red_circle: *${summaryResults.failed
+                    }* Tests failed \n\n ${summaryResults.skipped > 0
+                        ? `:fast_forward: *${summaryResults.skipped}* skipped`
+                        : ''
+                    } \n\n `,
             },
-        });
-    }
-}
-return [
-    {
-        type: "header",
-        text: {
-            type: "plain_text",
-            text: "🎭 *710 Labs Test Results*",
-            emoji: true,
         },
-    },
-    ...meta,
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `:white_check_mark: *${summaryResults.passed
-                }* Tests ran successfully \n\n :red_circle: *${summaryResults.failed
-                }* Tests failed \n\n ${summaryResults.skipped > 0
-                    ? `:fast_forward: *${summaryResults.skipped}* skipped`
-                    : ''
-                } \n\n `,
+        {
+            type: 'divider',
         },
-    },
-    {
-        type: 'divider',
-    },
-    ...fails,
-];
+        ...fails,
+    ];
 }
 
 
