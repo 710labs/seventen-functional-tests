5d0ac1b6cf4a170a6fe38521af1feac24afc00df
ladellerby <ladellerby@gmail.com>
2023-02-17 13:54:43 -0800

    - Adding test stubs for Auto Discount, Auto Split, and Auto Assign Fulfillment

--- tests/admin-actions.tests.spec.ts
+++ tests/admin-actions.tests.spec.ts
@@ -68,7 +68,112 @@ test.describe('Admin Split Order', () => {
 			await myAccountPage.logout()
 		})
 	})
-	test(`User Can Split Order @CA`, async ({ page, browserName }, workerInfo) => {
+	test(`User Can Split Order - Legacy Split @CA`, async ({ page, browserName }, workerInfo) => {
+		const adminLoginPage = new AdminLogin(page)
+		const editOrderPage = new EditOrderPage(page)
+		await test.step('Login Admin', async () => {
+			await adminLoginPage.login()
+		})
+		await test.step('Admin Split Order', async () => {
+			await editOrderPage.splitOrder(orderNumber, orderQuanity)
+		})
+	})
+	test.skip(`User Can Split Order - Auto Split @CA`, async ({ page, browserName }, workerInfo) => {
+		const adminLoginPage = new AdminLogin(page)
+		const editOrderPage = new EditOrderPage(page)
+		await test.step('Login Admin', async () => {
+			await adminLoginPage.login()
+		})
+		await test.step('Admin Split Order', async () => {
+			await editOrderPage.splitOrder(orderNumber, orderQuanity)
+		})
+	})
+})
+
+test.describe.skip('Auto Assign Fulfilment', () => {
+	const zipCode = '90210'
+	const orderQuanity = 6
+	var orderTotals
+	var orderNumber
+
+	test.beforeEach(async ({ page, browserName }, workerInfo) => {
+		test.skip(workerInfo.project.name === 'Mobile Chrome')
+		const apiContext = await request.newContext({
+			baseURL: `${process.env.BASE_URL}${process.env.QA_ENDPOINT}`,
+			extraHTTPHeaders: {
+				'x-api-key': `${process.env.API_KEY}`,
+			},
+		})
+		const address = '9779 Oak Pass Rd'
+		const ageGatePage = new AgeGatePage(page)
+		const listPassword = new ListPasswordPage(page)
+		const createAccountPage = new CreateAccountPage(page, apiContext)
+		const shopPage = new ShopPage(page, browserName, workerInfo)
+		const cartPage = new CartPage(page, apiContext, browserName, workerInfo, 1)
+		const schedulingPage = new SchedulingPage(page)
+		const orderReceived = new OrderReceivedPage(page)
+		const checkOutPage = new CheckoutPage(page, apiContext)
+		const myAccountPage = new MyAccountPage(page)
+
+		await ageGatePage.passAgeGate()
+		await listPassword.submitPassword('qatester')
+		await createAccountPage.create(
+			`test+${uuidv4()}@710labs-test.com`,
+			'test1234!',
+			zipCode,
+			1,
+			false,
+			address,
+		)
+		if (process.env.ADD_ADDRESS_BEFORE_CHECKOUT === 'true') {
+			await myAccountPage.addAddress()
+		}
+		await shopPage.addProductsToCart(6)
+		var cartTotals = await cartPage.verifyCart(zipCode)
+		await test.step('Verify Checkout Page Totals + Taxes', async () => {
+			orderTotals = await checkOutPage.confirmCheckout(zipCode, cartTotals, 1, true)
+		})
+
+		await test.step('Comfirm Order Details on /order-received', async () => {
+			orderNumber = await orderReceived.confirmOrderDetail(orderTotals)
+			await expect(orderNumber, 'Failed to create order').not.toBeNull()
+		})
+
+		await test.step('Logout Consumer', async () => {
+			await myAccountPage.logout()
+		})
+	})
+	test(`New Customer - No Previous Orders - Defaults to Pickup`, async ({
+		page,
+		browserName,
+	}, workerInfo) => {
+		const adminLoginPage = new AdminLogin(page)
+		const editOrderPage = new EditOrderPage(page)
+		await test.step('Login Admin', async () => {
+			await adminLoginPage.login()
+		})
+		await test.step('Admin Split Order', async () => {
+			await editOrderPage.splitOrder(orderNumber, orderQuanity)
+		})
+	})
+	test(`Existing Customer - Previou Order Pickup - Defaults to Pickup`, async ({
+		page,
+		browserName,
+	}, workerInfo) => {
+		const adminLoginPage = new AdminLogin(page)
+		const editOrderPage = new EditOrderPage(page)
+		await test.step('Login Admin', async () => {
+			await adminLoginPage.login()
+		})
+		await test.step('Admin Split Order', async () => {
+			await editOrderPage.splitOrder(orderNumber, orderQuanity)
+		})
+	})
+
+	test(`Existing Customer - Previou Order Delevery - Defaults to Delivery`, async ({
+		page,
+		browserName,
+	}, workerInfo) => {
 		const adminLoginPage = new AdminLogin(page)
 		const editOrderPage = new EditOrderPage(page)
 		await test.step('Login Admin', async () => {
---
+++ tests/cart-actions.spec.ts
@@ -0,0 +1,9 @@
+import { test } from '@playwright/test'
+
+test.describe.skip('Cart Auto Discounts', () => {
+	test('Customer Has Credit - Discount Applied', async ({ page }) => {})
+
+	test('Customer Has Credit - Order Total Equals Rec Limit - Discount Applied ', async ({
+		page,
+	}) => {})
+})
