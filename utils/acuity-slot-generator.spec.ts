41266312c4cf154ab5dc796502174517f8589423
ladellerby <ladellerby@gmail.com>
2023-02-24 13:34:26 -0800

    - organize github action scripts

--- utils/acuity-slot-generator.spec.ts
+++ utils/acuity-slot-generator.spec.ts
@@ -3,12 +3,13 @@ const csvFilePath = 'utils/delivery-slots.csv'
 let csvToJson = require('convert-csv-to-json')
 const { Parser } = require('json2csv')
 
-
 test.describe('Acuity Automation', () => {
 	let slots = csvToJson.fieldDelimiter(';').getJsonFromCsv(csvFilePath)
 	test.describe.configure({ mode: 'parallel' })
 	let page: Page
 	test.beforeAll(async ({ browser }) => {
+		//remove existing test failures csv
+		//add new one with expected values
 		page = await browser.newPage()
 		await test.step('Login to Acuity Scheduling', async () => {
 			await page.goto('https://login.squarespace.com/')
@@ -20,103 +21,105 @@ test.describe('Acuity Automation', () => {
 			])
 		})
 	})
+	test.afterEach(async ({ page }, testInfo) => {
+		if (testInfo.status !== testInfo.expectedStatus) {
+			//Parse test title and add entry to failures.csv
+			console.log(testInfo.title)
+			console.log(testInfo.annotations.)
+		}
+	})
 
 	for (let index = 0; index < slots.length; index++) {
-		try {
-			test(`Add Acuity Slots: ${slots[index].Partner_region_zone} - ${slots[index].DateOffered} - ${slots[index].TimeOffered} [${slots[index].Partner},${slots[index].Partner_region_zone},${slots[index].AppointmentID},${slots[index].URL},${slots[index].CalendarName},${slots[index].DateOffered},${slots[index].TimeOffered},${slots[index].LinkText},${slots[index].Availability}] @helper`, async ({}, workerInfo) => {
-				test.skip(workerInfo.project.name === 'Mobile Chrome')
-				await test.step(`Create Slot on ${slots[index].DateOffered} - ${slots[index].TimeOffered}`, async () => {
-					//Navigate to Zone
-					await page.goto(slots[index].URL)
-					await page.waitForTimeout(10000)
-					await page.goto(slots[index].URL)
+		test(`Add Acuity Slots: ${slots[index].Partner_region_zone} - ${slots[index].DateOffered} - ${slots[index].TimeOffered} [${slots[index].Partner},${slots[index].Partner_region_zone},${slots[index].AppointmentID},${slots[index].URL},${slots[index].CalendarName},${slots[index].DateOffered},${slots[index].TimeOffered},${slots[index].LinkText},${slots[index].Availability}] @helper`, async ({}, workerInfo) => {
+			test.skip(workerInfo.project.name === 'Mobile Chrome')
+			await test.step(`Create Slot on ${slots[index].DateOffered} - ${slots[index].TimeOffered}`, async () => {
+				//Navigate to Zone
+				await page.goto(slots[index].URL)
+				await page.waitForTimeout(10000)
+				await page.goto(slots[index].URL)
 
-					const popUp = await page
-						.frameLocator('[data-test="scheduling-iframe"]')
-						.getByRole('button', { name: 'Close' })
+				const popUp = await page
+					.frameLocator('[data-test="scheduling-iframe"]')
+					.getByRole('button', { name: 'Close' })
 
-					if (popUp != null) {
-						popUp.click()
-					}
-					// Start Create Slot
-					await page
-						.frameLocator('[data-test="scheduling-iframe"]')
-						.locator('#offer-class-btn')
-						.first()
-						.click()
-					// Select "Another Test Calendar"
-					await page
-						.frameLocator('[data-test="scheduling-iframe"]')
-						.locator('select[name="calendar"]')
-						.selectOption({ label: slots[index].CalendarName })
-					// Date Selector
-					// Select Day of Month
-					await page
-						.frameLocator('[data-test="scheduling-iframe"]')
-						.locator('#date-input')
-						.fill(`${slots[index].DateOffered}`)
+				if (popUp != null) {
+					popUp.click()
+				}
+				// Start Create Slot
+				await page
+					.frameLocator('[data-test="scheduling-iframe"]')
+					.locator('#offer-class-btn')
+					.first()
+					.click()
+				// Select "Another Test Calendar"
+				await page
+					.frameLocator('[data-test="scheduling-iframe"]')
+					.locator('select[name="calendar"]')
+					.selectOption({ label: slots[index].CalendarName })
+				// Date Selector
+				// Select Day of Month
+				await page
+					.frameLocator('[data-test="scheduling-iframe"]')
+					.locator('#date-input')
+					.fill(`${slots[index].DateOffered}`)
 
-					// Select Time
-					await page
-						.frameLocator('[data-test="scheduling-iframe"]')
-						.locator('[placeholder="Ex\\. 9\\:00am"]')
-						.click()
-					await page
-						.frameLocator('[data-test="scheduling-iframe"]')
-						.locator('[placeholder="Ex\\. 9\\:00am"]')
-						.fill(`${slots[index].TimeOffered}`)
-					// Save Class
-					await Promise.all([
-						page.waitForNavigation(/*{ url: 'https://koi-mandolin-afct.squarespace.com/config/scheduling/appointments.php?action=editAppointmentType&id=27879714' }*/),
-						page.frameLocator('[data-test="scheduling-iframe"]').locator('text=Save Class').click(),
-					])
+				// Select Time
+				await page
+					.frameLocator('[data-test="scheduling-iframe"]')
+					.locator('[placeholder="Ex\\. 9\\:00am"]')
+					.click()
+				await page
+					.frameLocator('[data-test="scheduling-iframe"]')
+					.locator('[placeholder="Ex\\. 9\\:00am"]')
+					.fill(`${slots[index].TimeOffered}`)
+				// Save Class
+				await Promise.all([
+					page.waitForNavigation(/*{ url: 'https://koi-mandolin-afct.squarespace.com/config/scheduling/appointments.php?action=editAppointmentType&id=27879714' }*/),
+					page.frameLocator('[data-test="scheduling-iframe"]').locator('text=Save Class').click(),
+				])
 
-					//Edit Capacity
-					//Select Slot
-					//Prevent 12PM and 2PM collison
-					await page
-						.frameLocator('[data-test="scheduling-iframe"]')
-						.getByText(`${slots[index].LinkText}`, { exact: true })
-						.click()
+				//Edit Capacity
+				//Select Slot
+				//Prevent 12PM and 2PM collison
+				await page
+					.frameLocator('[data-test="scheduling-iframe"]')
+					.getByText(`${slots[index].LinkText}`, { exact: true })
+					.click()
 
-					// Click Edit
-					await page.frameLocator('[data-test="scheduling-iframe"]').locator('text=Edit').click()
+				// Click Edit
+				await page.frameLocator('[data-test="scheduling-iframe"]').locator('text=Edit').click()
 
-					// Edit Capacity Value
-					await page
-						.frameLocator('[data-test="scheduling-iframe"]')
-						.locator('text=Max number of people for this class >> input[name="group_max"]')
-						.click()
-					await page
-						.frameLocator('[data-test="scheduling-iframe"]')
-						.locator('text=Max number of people for this class >> input[name="group_max"]')
-						.fill(slots[index].Availability)
+				// Edit Capacity Value
+				await page
+					.frameLocator('[data-test="scheduling-iframe"]')
+					.locator('text=Max number of people for this class >> input[name="group_max"]')
+					.click()
+				await page
+					.frameLocator('[data-test="scheduling-iframe"]')
+					.locator('text=Max number of people for this class >> input[name="group_max"]')
+					.fill(slots[index].Availability)
 
-					// Save Slot
-					await Promise.all([
-						page.waitForNavigation(/*{ url: 'https://koi-mandolin-afct.squarespace.com/config/scheduling/appointments.php?action=editAppointmentType&id=27879714' }*/),
-						page
-							.frameLocator('[data-test="scheduling-iframe"]')
-							.locator('text=Save Changes')
-							.click(),
-					])
-				})
+				// Save Slot
+				await Promise.all([
+					page.waitForNavigation(/*{ url: 'https://koi-mandolin-afct.squarespace.com/config/scheduling/appointments.php?action=editAppointmentType&id=27879714' }*/),
+					page.frameLocator('[data-test="scheduling-iframe"]').locator('text=Save Changes').click(),
+				])
 			})
-		} catch (error) {
-			//Partner;Partner_region_zone;Appointment ID;URL;Calendar Name;Date Offered;Time Offered;Link Text;Availability
-			const fields = [
-				'Partner',
-				'Partner_region_zone',
-				'Appointment ID',
-				'URL',
-				'Calendar Name',
-				'Date Offered',
-				'Link Text',
-				'Availability',
-			]
-			const opts = { fields }
-			const parser = new Parser(opts)
-			const csv = parser.parse(slots[index])
-		}
+		})
 	}
 })
+
+// //Partner;Partner_region_zone;Appointment ID;URL;Calendar Name;Date Offered;Time Offered;Link Text;Availability
+// const fields = [
+// 	'Partner',
+// 	'Partner_region_zone',
+// 	'Appointment ID',
+// 	'URL',
+// 	'Calendar Name',
+// 	'Date Offered',
+// 	'Link Text',
+// 	'Availability',
+// ]
+// const opts = { fields }
+// const parser = new Parser(opts)
+// const csv = parser.parse(slots[index])
