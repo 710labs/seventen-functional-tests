bc34104e7ca78fc66afeb4bf5170c1df291e6691
ladellerby <ladellerby@gmail.com>
2023-04-03 03:10:50 -0700

    fix: update dev env slack channels + add out of state mi e2e test

--- tests/e2e-tests/michigan-order.spec.ts
+++ tests/e2e-tests/michigan-order.spec.ts
@@ -35,7 +35,7 @@ var orders = [
 	],
 ]
 
-test(`Basic Order - New Customer - In State - #medical @MI`, async ({
+test(`Basic Order - New Customer - In State - #recreational @MI`, async ({
 	page,
 	browserName,
 }, workerInfo) => {
@@ -71,8 +71,80 @@ test(`Basic Order - New Customer - In State - #medical @MI`, async ({
 			faker.datatype.number({ min: 1, max: 28 }),
 			faker.datatype.number({ min: 10, max: 12 }),
 			faker.datatype.number({ min: 1975, max: 2001 }),
-			~~faker.phone.phoneNumber('##########'),
-			'medical',
+			faker.datatype.number({ min: 1111111111, max: 9999999999 }),
+			'recreational',
+			address,
+			faker.datatype.number({ min: 11111111, max: 99999999 }).toString(),
+			faker.datatype.number({ min: 11111111, max: 99999999 }).toString(),
+		)
+	})
+	await test.step(`Enter Fulfillment Method`, async () => {
+		await page.locator('#fulfillmentElement').getByText('Pickup', { exact: true }).click()
+		await page.getByRole('button', { name: 'Submit' }).click()
+	})
+
+	await test.step(`Add Products`, async () => {
+		var order = await faker.datatype.number({ min: 0, max: orders.length - 1 })
+		await test.step(`Load Cart - Order #${order}`, async () => {
+			await shopPage.addProductListToCart(orders[order])
+		})
+	})
+
+	await test.step(`Navigate to Cart`, async () => {
+		await shopPage.goToCart()
+	})
+
+	await test.step(`Navigate to Checkout`, async () => {
+		await cartPage.goToCheckout()
+	})
+
+	await test.step(`Choose Acuity Slot`, async () => {
+		await checkOutPage.selectSlot()
+	})
+
+	await test.step(`Complete Order`, async () => {
+		await checkOutPage.placeOrderButton.click()
+	})
+})
+
+test(`Basic Order - New Customer - Out Of State - #recreational @MI`, async ({
+	page,
+	browserName,
+}, workerInfo) => {
+	const address = '123 Broadway New York'
+	const apiContext = await request.newContext({
+		baseURL: `${process.env.BASE_URL}${process.env.QA_ENDPOINT}`,
+		extraHTTPHeaders: {
+			'x-api-key': `${process.env.API_KEY}`,
+		},
+	})
+	const ageGatePage = new AgeGatePage(page)
+	const listPassword = new ListPasswordPage(page)
+	const createAccountPage = new CreateAccountPage(page, apiContext)
+	const shopPage = new ShopPage(page, browserName, workerInfo)
+	const cartPage = new CartPage(page, apiContext, browserName, workerInfo, 1)
+	const checkOutPage = new CheckoutPage(page, apiContext)
+	test.skip(workerInfo.project.name === 'Mobile Chrome')
+
+	await test.step(`Pass Age Gate`, async () => {
+		await ageGatePage.passAgeGate()
+	})
+
+	await test.step(`Enter List Password`, async () => {
+		await listPassword.submitPassword('qatester')
+	})
+
+	await test.step(`Create Account`, async () => {
+		await createAccountPage.createMichiganCustomer(
+			faker.name.firstName(),
+			faker.name.lastName(),
+			faker.internet.email(),
+			faker.internet.password(),
+			faker.datatype.number({ min: 1, max: 28 }),
+			faker.datatype.number({ min: 10, max: 12 }),
+			faker.datatype.number({ min: 1975, max: 2001 }),
+			faker.datatype.number({ min: 1111111111, max: 9999999999 }),
+			'recreational',
 			address,
 			faker.datatype.number({ min: 11111111, max: 99999999 }).toString(),
 			faker.datatype.number({ min: 11111111, max: 99999999 }).toString(),
