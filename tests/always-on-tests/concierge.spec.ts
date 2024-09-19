581ced8561c49c4d47456cf9881972e361041e75
brconnelly <bgconnelly29@gmail.com>
2024-09-19 15:03:07 -0300

    add Med user checkout

--- tests/always-on-tests/concierge.spec.ts
+++ tests/always-on-tests/concierge.spec.ts
@@ -10,33 +10,6 @@ import { ConciergeCreateUser } from '../../models/concierge/concierge-create-use
 test.describe('Concierge Tests', () => {
 	test.setTimeout(60000) // Set the timeout for all tests in this file
 	test.describe.configure({ mode: 'parallel' })
-
-	// test('MED New User - Happy Path test - Register & Checkout Med-Only Products', async ({
-	// 	page,
-	// }) => {
-	// 	const homePageLogin = new HomePageLogin(page)
-	// 	const homePageActions = new HomePageActions(page)
-	// 	const checkoutPage = new CheckoutPage(page)
-	// 	const orderConfirmation = new OrderConfirmationPage(page)
-
-	// 	// Verify that store homepage loads
-	// 	await homePageLogin.verifyUserSignInModalAppears(page)
-	// 	// register new user
-	// 	await homePageLogin.registerNewUser(page)
-	// 	await homePageLogin.verifyShopLoadsAfterSignIn(page)
-	// 	// add adress for new user account
-	// 	await homePageActions.enterAddress(page)
-	// 	// verify that homepage loads again
-	// 	await homePageLogin.verifyShopLoadsAfterSignIn(page)
-	// 	// add products to cart
-	// 	await homePageActions.medAddProductsToCartUntilMinimumMet(page)
-	// 	// verify that checkout page loads
-	// 	await checkoutPage.verifyCheckoutPageLoads(page)
-	// 	// enter in user info on checkoutpage
-	// 	await checkoutPage.enterInfoForCheckout(page)
-	// 	// verify order confirmation loads
-	// 	await orderConfirmation.verifyOrderConfirmationPageLoads(page)
-	// })
 	test('Existing user -- Sign In & Sign Out', async ({ page }) => {
 		const conciergeLogin = new ConciergeLogin(page)
 		const accountPage = new AccountPage(page)
@@ -81,6 +54,47 @@ test.describe('Concierge Tests', () => {
 		await homePageLogin.verifyShopLoadsAfterSignIn(page)
 		// add products to cart
 		await homePageActions.recAddProductsToCartUntilMinimumMet(page)
+		// verify that checkout page loads
+		await checkoutPage.verifyCheckoutPageLoads(page)
+		// enter in user info on checkoutpage
+		await checkoutPage.enterInfoForCheckout(page)
+		// verify order confirmation loads
+		await orderConfirmation.verifyOrderConfirmationPageLoads(page)
+	})
+	test('MED New User - Happy Path test - Create New User & Checkout', async ({ page }) => {
+		const conciergeLogin = new ConciergeLogin(page)
+		const homePageLogin = new HomePageLogin(page)
+		const homePageActions = new HomePageActions(page)
+		const checkoutPage = new CheckoutPage(page)
+		const orderConfirmation = new OrderConfirmationPage(page)
+		const conciergeCreateUser = new ConciergeCreateUser(page)
+
+		const now = new Date()
+		// Construct the timestamp string with date and time
+		const timestamp = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(
+			now.getDate(),
+		).padStart(2, '0')}_${String(now.getHours()).padStart(2, '0')}-${String(
+			now.getMinutes(),
+		).padStart(2, '0')}-${String(now.getSeconds()).padStart(2, '0')}-${String(
+			now.getMilliseconds(),
+		).padStart(3, '0')}`
+		// enter a unique test email for test user
+		const newEmail = `test_auto_${timestamp}@test.com`
+		const newPassword = `Test710!`
+		// login admin to WP dashboard
+		await conciergeCreateUser.loginAdmin(page)
+		//create new user as admin
+		await conciergeCreateUser.createNewUser(page, newEmail, newPassword)
+		//login to shop as new user
+		await conciergeLogin.loginNewUserCreated(page, newEmail, newPassword)
+		//verify that shop loads upon log in
+		await homePageLogin.verifyShopLoadsAfterSignIn(page)
+		// add adress for new user account
+		await homePageActions.enterAddress(page, 'concierge')
+		// verify that homepage loads again
+		await homePageLogin.verifyShopLoadsAfterSignIn(page)
+		// add products to cart
+		await homePageActions.medAddProductsToCartUntilMinimumMet(page)
 		// verify that checkout page loads
 		await checkoutPage.verifyCheckoutPageLoads(page)
 		// enter in user info on checkoutpage
