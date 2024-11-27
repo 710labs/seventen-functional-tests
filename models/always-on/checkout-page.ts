20114e25370bf0a0020e8a4e77cfd8793148578b
brconnelly <bgconnelly29@gmail.com>
2024-11-27 13:25:39 -0300

    update checkout

--- models/always-on/checkout-page.ts
+++ models/always-on/checkout-page.ts
@@ -325,12 +325,15 @@ export class CheckoutPage {
 			//enter false password to verify enforcement
 			await this.passwordCheckoutField.click()
 			await this.passwordCheckoutField.fill('fakepassword')
+			await page.waitForTimeout(500)
 			await this.submitPasswordButton.click()
 			await expect(this.passwordError).toHaveText('Please verify password')
+			await page.waitForTimeout(1500)
 			//enter correct password
 			await this.passwordCheckoutField.click()
 			const password = process.env.CHECKOUT_PASSWORD || ''
 			await this.passwordCheckoutField.fill(password)
+			await page.waitForTimeout(500)
 			await this.submitPasswordButton.click()
 			await page.waitForTimeout(1500)
 			// place order once password has been entered
@@ -554,12 +557,15 @@ export class CheckoutPage {
 			//enter false password to verify enforcement
 			await this.passwordCheckoutField.click()
 			await this.passwordCheckoutField.fill('fakepassword')
+			await page.waitForTimeout(500)
 			await this.submitPasswordButton.click()
 			await expect(this.passwordError).toHaveText('Please verify password')
+			await page.waitForTimeout(1500)
 			//enter correct password
 			await this.passwordCheckoutField.click()
 			const password = process.env.CHECKOUT_PASSWORD || ''
 			await this.passwordCheckoutField.fill(password)
+			await page.waitForTimeout(500)
 			await this.submitPasswordButton.click()
 			await page.waitForTimeout(1500)
 			// place order once password has been entered
