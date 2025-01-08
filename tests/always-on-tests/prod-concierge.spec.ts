89375c395077360f99b8f4a76ba014fd5df743a7
brconnelly <bgconnelly29@gmail.com>
2025-01-08 13:29:24 -0300

    concierge & live

--- tests/always-on-tests/prod-concierge.spec.ts
+++ tests/always-on-tests/prod-concierge.spec.ts
@@ -4,54 +4,57 @@ import { AccountPage } from '../../models/always-on/account-page.ts'
 import { HomePageActions } from '../../models/always-on/homepage-actions.ts'
 import { CheckoutPage } from '../../models/always-on/checkout-page.ts'
 import { OrderConfirmationPage } from '../../models/always-on/order-confirmation.ts'
+import { ConciergeLogin } from '../../models/concierge/concierge-login.ts'
 require('dotenv').config('.env')
 
-test.describe('Live Tests', () => {
+test.describe('PROD CONCIERGE Tests', () => {
 	test.setTimeout(240000) // Set the timeout for all tests in this file
 	test.describe.configure({ mode: 'parallel' })
 	var apiContext: APIRequestContext
-	const prodLiveURL = process.env.PROD_LIVE_URL || ''
-	const prodLiveUsername = process.env.PROD_LIVE_USERNAME || ''
-	const prodLivePassword = process.env.PROD_LIVE_PASSWORD || ''
+	const prodConciergeURL = process.env.PROD_CONCIERGE_URL || ''
+	const prodConciergeUsername = process.env.PROD_CONCIERGE_USERNAME || ''
+	const prodConciergePassword = process.env.PROD_CONCIERGE_PASSWORD || ''
 	const NEWalwaysOnPassword = process.env.NEW_ALWAYS_ON_PASSWORD || ''
 
-	console.log(`------- \n URL being tested: ${prodLiveURL} -------- \n `)
+	console.log(`------- \n URL being tested: ${prodConciergeURL} -------- \n `)
 	test(
-		'MED EXISTING User - Happy Path test - Sign In, Add Products to Cart, but DO NOT CHECKOUT',
+		'Prod check - MED EXISTING User - Happy Path test - Sign In, Add Products to Cart, but DO NOT CHECKOUT',
 		{ tag: ['@medical'] },
 		async ({ page }) => {
+			const conciergeLogin = new ConciergeLogin(page)
 			const homePageLogin = new HomePageLogin(page)
 			const homePageActions = new HomePageActions(page)
 			const checkoutPage = new CheckoutPage(page)
+			const orderConfirmation = new OrderConfirmationPage(page)
 			const accountPage = new AccountPage(page)
-
-			// Verify that store homepage loads
-			await homePageLogin.verifyUserSignInModalAppears(page)
-			// Login Existing Prod User
-			await homePageLogin.loginExistingUser(
+			await conciergeLogin.loginUser(
 				page,
 				'wrongpassword',
-				prodLiveUsername,
-				prodLivePassword,
+				prodConciergeUsername,
+				prodConciergePassword,
 			)
-
+			//verify that shop loads upon log in
 			await homePageLogin.verifyShopLoadsAfterSignIn(page)
 			const address = '440 N Rodeo Dr, Beverly Hills, CA 90210'
 			const newAddress = '2919 S La Cienega Blvd, Culver City, CA'
 			// add adress for new user account
-			//await homePageActions.enterAddress(page, 'live', address)
+			//await homePageActions.enterAddress(page, 'concierge', address)
 			// verify that homepage loads again
 			await homePageLogin.verifyShopLoadsAfterSignIn(page)
 			//
 			// TODO: CLEAR CART IF PRODUCTS EXIST
 			await homePageActions.clearProductsFromCart(page)
 			// add products to cart
-			await homePageActions.liveMedAddProductsToCartUntilMinimumMet(page)
-			//await homePageActions.newLiveMedAddProductsToCartUntilMinimumMet(page)
+			await homePageActions.conciergeMedAddProductsToCartUntilMinimumMet(page)
 			// verify that checkout page loads
 			await checkoutPage.verifyCheckoutPageLoads(page)
 			// enter in user info on checkoutpage
-			await checkoutPage.newMedEnterInfoForCheckoutAndEdit(page, prodLiveURL, address, newAddress)
+			await checkoutPage.newMedEnterInfoForCheckoutAndEdit(
+				page,
+				prodConciergeURL,
+				address,
+				newAddress,
+			)
 			// verify order confirmation loads
 			//
 			//
