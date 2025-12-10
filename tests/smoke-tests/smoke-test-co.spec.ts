ce18c8938f4e2544d690e2ff41bc0edf0c80b5b9
brconnelly <bgconnelly29@gmail.com>
2025-12-10 16:53:44 -0300

    update prod tests to include Med carts as well

--- tests/smoke-tests/smoke-test-co.spec.ts
+++ tests/smoke-tests/smoke-test-co.spec.ts
@@ -21,7 +21,7 @@ test.describe('Basic Acceptance Tests CO', () => {
 	var splitOrderNumber: string
 	var cartTotals: any
 
-	test(`Basic Acceptance Test @smoke`, async ({ page, browserName, context }, workerInfo) => {
+	test(`Basic Acceptance Test - Medical @medical @smoke`, async ({ page, browserName, context }, workerInfo) => {
 		test.skip(workerInfo.project.name === 'Mobile Chrome')
 		const apiContext = await request.newContext({
 			baseURL: `${process.env.BASE_URL}${process.env.QA_ENDPOINT}`,
@@ -78,7 +78,98 @@ test.describe('Basic Acceptance Tests CO', () => {
 		})
 
 		await test.step('Add Products to Cart', async () => {
-			await shopPage.addProductsToCart(orderQuanity, mobile, 'Pickup')
+			await shopPage.addProductsToCart(orderQuanity, mobile, 'Pickup', 'Medical')
+			cartTotals = await cartPage.verifyCart(address.zipcode)
+		})
+
+		await test.step('Choose Fulfillment Slot + Verify Checkout', async () => {
+			await checkOutPage.confirmCheckout(address.zipcode, cartTotals, 1, true, address)
+		})
+
+		await test.step('Comfirm Order Details on /order-received', async () => {
+			orderNumber = await orderReceived.getOrderNumber()
+			expect(orderNumber, 'Failed to create order').not.toBeNull()
+			//write order number to file to use for cancel order via API
+			writeFileSync('order_id.txt', String(orderNumber), { encoding: 'utf-8' })
+			console.log(`✅ Wrote order_id.txt → ${orderNumber}`)
+		})
+		await test.step('Logout Consumer', async () => {
+			await myAccountPage.logout()
+		})
+
+		await test.step('Login Admin', async () => {
+			await adminLoginPage.login()
+		})
+		await test.step('Admin Split Order', async () => {
+			splitOrderNumber = await editOrderPage.splitOrder(orderNumber, orderQuanity)
+			//write split order number to file to use for cancel order via API
+			writeFileSync('split_order_id.txt', String(splitOrderNumber), { encoding: 'utf-8' })
+			console.log(`✅ Wrote split_order_id.txt → ${splitOrderNumber}`)
+		})
+		await test.step('Cancel Order', async () => {
+			await editOrderPage.cancelOrder(orderNumber)
+			await editOrderPage.cancelOrder(splitOrderNumber)
+		})
+	})
+
+	test(`Basic Acceptance Test - REC @recreational @smoke`, async ({ page, browserName, context }, workerInfo) => {
+		test.skip(workerInfo.project.name === 'Mobile Chrome')
+		const apiContext = await request.newContext({
+			baseURL: `${process.env.BASE_URL}${process.env.QA_ENDPOINT}`,
+			extraHTTPHeaders: {
+				'x-api-key': `${process.env.API_KEY}`,
+			},
+		})
+		await context.addCookies([
+			{
+				name: 'vipChecker',
+				value: '3',
+				domain: process.env.BASE_URL?.replace('https://', ''),
+				path: '/',
+			},
+		])
+		const address = faker.helpers.arrayElement(coloradoAddressess)
+		var fakeFirstName = faker.name.firstName() + '_Test'
+		var fakeLastName = faker.name.lastName() + '_Test'
+		var fakeEmail = faker.internet.email(fakeFirstName, fakeLastName, 'test710labstest.com') // 'Jeanne_Doe88@example.fakerjs.dev'
+
+		const email = `test+${uuidv4()}@710labs-test.com`
+		const ageGatePage = new AgeGatePage(page)
+		const listPassword = new ListPasswordPage(page)
+		const createAccountPage = new CreateAccountPage(page, apiContext)
+		const myAccountPage = new MyAccountPage(page)
+		const shopPage = new ShopPage(page, browserName, workerInfo)
+		const cartPage = new CartPage(page, apiContext, browserName, workerInfo, 1)
+		const checkOutPage = new CheckoutPage(page, apiContext)
+		const adminLoginPage = new AdminLogin(page)
+		const editOrderPage = new EditOrderPage(page)
+		const orderReceived = new OrderReceivedPage(page)
+		var mobile = workerInfo.project.name === 'Mobile Chrome' ? true : false
+
+		await test.step('Pass Age Gate', async () => {
+			await ageGatePage.passAgeGate()
+		})
+
+		await test.step('Enter List Password', async () => {
+			await listPassword.submitPassword('qatester')
+		})
+
+		await test.step('Create Account', async () => {
+			await createAccountPage.create(
+				fakeFirstName,
+				fakeLastName,
+				fakeEmail,
+				'test1234',
+				address.zipcode,
+				1,
+				false,
+				address.fullAddress,
+				'CO',
+			)
+		})
+
+		await test.step('Add Products to Cart', async () => {
+			await shopPage.addProductsToCart(orderQuanity, mobile, 'Pickup', 'Recreational')
 			cartTotals = await cartPage.verifyCart(address.zipcode)
 		})
 
